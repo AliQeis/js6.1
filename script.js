e4dd9481a0652d7a75ebe78c8971197bed5d2dcb
jschaik <9020512@student.zadkine.nl>
2022-10-13 13:58:36 +0200

    js6.1

---
+++ script.js
@@ -0,0 +1,9 @@
+function changecolor(){
+    alert("het werkt");
+    console.log("het werkt");
+}
+function calculator(num, num2){
+    let answer = num + num2 ;
+    console.log(answer);
+    alert(answer);
+}
